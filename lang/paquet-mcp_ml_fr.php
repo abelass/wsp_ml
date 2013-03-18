1d2600244bafeb3d63d1de64cf178702ccbc4d46
abelass <websolutions@mychacra.net>
2013-03-18 09:45:30 +0100

    orthographe

--- lang/paquet-mcp_ml_fr.php
+++ lang/paquet-mcp_ml_fr.php
@@ -7,7 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'mcp_ml_description' => 'Pack Multilingue',
-	'mcp_ml_nom' => 'My Chacra - Pack Miltilingue',
+	'mcp_ml_nom' => 'My Chacra - Pack Multilingue',
 	'mcp_ml_slogan' => 'Int&egrave;gre le multilinguisme',
 );
 
